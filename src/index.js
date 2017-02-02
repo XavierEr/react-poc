2da4e289f02971aa8f0a1ef2187c804abd3839f6
Xavier Er <xavier_er@live.com.sg>
2017-02-02 16:27:43 +0800

    Add hmr

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { AppContainer } from 'react-hot-loader';
+
 import Routes from './config/Routes';
 
 ReactDOM.render(
-    <Routes />,
+    <AppContainer>
+        <Routes />
+    </AppContainer>,
     document.getElementById('app')
 );
+
+if (module.hot) {
+    module.hot.accept('./config/Routes', () => {
+        ReactDOM.render(
+            <AppContainer>
+                <Routes />
+            </AppContainer>,
+            document.getElementById('app')
+        );
+    });
+}
